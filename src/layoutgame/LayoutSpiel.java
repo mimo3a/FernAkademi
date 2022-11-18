126dda77cc16591eff7225b68a6d8c0d8091dbe2
Oleksandr <mimo3a@gmail.com>
2022-11-18 22:33:51 +0100

    last version

--- src/layoutgame/LayoutSpiel.java
+++ src/layoutgame/LayoutSpiel.java
@@ -1,6 +1,5 @@
 package layoutgame;
 
-
 import java.awt.BorderLayout;
 import java.awt.ComponentOrientation;
 import java.awt.Dimension;
@@ -10,10 +9,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
-
+import java.util.concurrent.ArrayBlockingQueue;
 
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -22,311 +22,336 @@ import javax.swing.JRadioButton;
 import javax.swing.ScrollPaneLayout;
 import javax.swing.border.TitledBorder;
 
-public class LayoutSpiel extends JFrame{
+public class LayoutSpiel extends JFrame {
 	// die Komponenten
-	// funf Schaltflachen für die Ausgabe	
+	// funf Schaltflachen für die Ausgabe
 	JButton button1, button2, button3, button4, button5;
-	
-	// vier Label fur Gridauswahlpanel
-	JLabel blabel1, blabel2, blabel3, blabel4;
-	//drei Radiobuttons fur die Layoutauswahlpanel und GridAuswahlpanel
-	JRadioButton flow, border, grid, gridNullEins, gridZweiZwei;
-	// vier ComboBox fur das Gridauswahlpanel
-	JComboBox<String> comboButton1, comboButton2, comboButton3, comboButton4;	
-	//	Alle Panele
+	//und eine für BorderLayout panel
+	JButton goBorderLayout;
+
+	// fünf Label fur Borderpanel
+	JLabel blabel1, blabel2, blabel3, blabel4, blabel5;
+	//zwei label fur gridPanel
+	JLabel glabel1, glabel2;
+	// drei Radiobuttons fur die Layoutauswahlpanel
+	JRadioButton flow, border,grid; 
+	// zwei Konbination feldern fur das GridPanel
+	JComboBox reihe, spalte;
+	// funf Kuntrolkastchen fur das BorderPanel
+	JCheckBox north, south, west, east, center;
+
+	// Alle Panele
 	JPanel generalpanel, changepanel, borderpanel, gridpanel, viewpanel;
-	
+
 	// die Methode erzeugt das Panel für die Layoutsarten
-	
+
 	public JPanel createChangePanel() {
-		
+
 		JPanel tempPanel = new JPanel();
 		flow = new JRadioButton("Flow Layout");
 		border = new JRadioButton("Border Layout");
 		grid = new JRadioButton("Grid Layout");
 		flow.setSelected(true);
-		
+
 		ButtonGroup gruppe = new ButtonGroup();
 		gruppe.add(flow);
 		gruppe.add(border);
 		gruppe.add(grid);
-		
-		tempPanel.setLayout(new GridLayout(0,1,10,10));
+
+		tempPanel.setLayout(new GridLayout(0, 1, 10, 10));
 		tempPanel.setBorder(new TitledBorder("Change the layout"));
-		
+
 		tempPanel.add(flow);
 		tempPanel.add(border);
 		tempPanel.add(grid);
-		
+
 		MyListener listener = new MyListener();
 		flow.addActionListener(listener);
 		border.addActionListener(listener);
 		grid.addActionListener(listener);
-		
+
 		return tempPanel;
-				
+
 	}
-	
-	// die Methode erzeugt das Panel für den BorderLayout
-	
+
+	// die Methode erzeugt das Panel für das Borderlayoutpanel
 	public JPanel createborderpanel() {
-		
+
 		JPanel temPanel = new JPanel();
-		// 
-		temPanel.setLayout(new GridLayout(4,2,10,10));
-		
+		temPanel.setLayout(new GridLayout(0, 2, 10, 0));
+
 		blabel1 = new JLabel("button 1");
 		blabel2 = new JLabel("button 2");
 		blabel3 = new JLabel("button 3");
 		blabel4 = new JLabel("button 4");
+		blabel5 = new JLabel("button 5");
+
+		north = new JCheckBox("NORTH");
+		south = new JCheckBox("SOUTH");
+		west = new JCheckBox("WEST");
+		east = new JCheckBox("EAST");
+		center = new JCheckBox("CENTER");
 		
-		String comboarray [] = {"NORTH", "SOUTH", "WEST", "EAST", "CENTER"};
-		comboButton1 = new JComboBox<String>(comboarray);
-		comboButton2 = new JComboBox<String>(comboarray);
-		comboButton3 = new JComboBox<String>(comboarray);
-		comboButton4 = new JComboBox<String>(comboarray);
-		
+		goBorderLayout = new JButton("Go");
+
 		temPanel.add(blabel1);
-		temPanel.add(comboButton1);
+		temPanel.add(north);
 		temPanel.add(blabel2);
-		temPanel.add(comboButton2);
+		temPanel.add(south);
 		temPanel.add(blabel3);
-		temPanel.add(comboButton3);
+		temPanel.add(west);
 		temPanel.add(blabel4);
-		temPanel.add(comboButton4);
+		temPanel.add(east);
+		temPanel.add(blabel5);
+		temPanel.add(center);
+		temPanel.add(goBorderLayout);
+
 		MyListener listener = new MyListener();
-		comboButton1.setActionCommand("button1");
-		comboButton1.addActionListener(listener);
-		comboButton2.setActionCommand("button2");
-		comboButton2.addActionListener(listener);
-		comboButton3.setActionCommand("button3");
-		comboButton3.addActionListener(listener);
-		comboButton4.setActionCommand("button4");
-		comboButton4.addActionListener(listener);
-		
-		
-		setEnableBorder(false);
-		
+		north.addActionListener(listener);
+		south.addActionListener(listener);
+		west.addActionListener(listener);
+		east.addActionListener(listener);
+		center.addActionListener(listener);
+		goBorderLayout.addActionListener(listener);
+		goBorderLayout.setActionCommand("go");
+	
 		temPanel.setBorder(new TitledBorder("Change BorderLayout"));
-		temPanel.setEnabled(false);		
-		
+//		das Panel inaktiv machen
+		temPanel.setEnabled(false);
+
 		return temPanel;
-		
+
 	}
-	
-	// die Methode erzeugt das Panel  zum Auswählen der Anzahl der Zeilen
-	//	und Spalten Gridpanels
-	
+
+	// die Methode erzeugt das Panel zum Auswählen der Anzahl der Zeilen
+	// und Spalten Gridpanels
+
 	public JPanel creategridpanel() {
 		JPanel temPanel = new JPanel();
-		
-		gridNullEins = new JRadioButton("(0, 1)");
-		gridZweiZwei = new JRadioButton("(2,2)");
-		
-		temPanel.setLayout(new GridLayout(0,1));
+
+		glabel1 = new JLabel("Reihe");
+		glabel2 = new JLabel("Spalte");
+		String [] tempArr = {"0","1","2","3"};
+		reihe = new JComboBox<String>(tempArr);		
+		spalte = new JComboBox<String>(tempArr);		
+
+		temPanel.setLayout(new GridLayout(0, 2,0,10));
 		temPanel.setBorder(new TitledBorder("Grid"));
+
+		temPanel.add(glabel1);
+		temPanel.add(reihe);
+		temPanel.add(glabel2);
+		temPanel.add(spalte);
 		
-		ButtonGroup gruppe = new ButtonGroup();
-		gruppe.add(gridNullEins);
-		gruppe.add(gridZweiZwei);
-		
-		temPanel.add(gridNullEins);
-		temPanel.add(gridZweiZwei);
-		
+		MyListener listener = new MyListener();
+		reihe.addActionListener(listener);
+		spalte.addActionListener(listener);		
+
 		setEnableGrid(false);
-		
+
 		return temPanel;
 	}
-	
-	//die Methode zum Erstellen eines Ausgabebereichs	
-	
+
+	// die Methode zum Erstellen eines Ausgabebereichs
+
 	public JPanel createviewpanel() {
-		
-		JPanel temPanel = new JPanel();						
+
+		JPanel temPanel = new JPanel();
 		temPanel.setBorder(new TitledBorder("view panel"));
-		temPanel.setPreferredSize(new Dimension(250,150));
-		
+		temPanel.setPreferredSize(new Dimension(250, 150));
+
 		return temPanel;
-		}
-	
-	// die Methode macht die Panel erreichbar oder nicht
+	}
+
+	// die Methode macht das Borderlayoutspanel erreichbar oder nicht
 	public void setEnableBorder(Boolean bool) {
+
+		north.setEnabled(bool);
+		south.setEnabled(bool);
+		west.setEnabled(bool);
+		east.setEnabled(bool);
+		center.setEnabled(bool);
+		goBorderLayout.setEnabled(bool);
 		
-		comboButton1.setEnabled(bool);
-		comboButton2.setEnabled(bool);
-		comboButton3.setEnabled(bool);
-		comboButton4.setEnabled(bool);
 	}
-	// die Methode macht die Panel erreichbar oder nicht
+
+	// die Methode macht das Gridpanel erreichbar oder nicht
 	public void setEnableGrid(Boolean bool) {
-		gridNullEins.setEnabled(bool);
-		gridZweiZwei.setEnabled(bool);
+		reihe.setEnabled(bool);
+		spalte.setEnabled(bool);
 	}
-	//Die Methode löscht zuerst das Panel, legt dann das BorderLayout 
-	//dafür fest und fügt die Elemente hinzu aktualisiert dann die Layuts von general 
-	//und view Panels
-	public void setBorderLayout() {
+
+	// Die Methode löscht zuerst das Panel, legt dann das BorderLayout
+	// dafür fest und fügt die Elemente hinzu aktualisiert dann die Layuts von
+	// general
+	// und view Panels
+	public void setBorderLayout(int [] arr) {
 		viewpanel.removeAll();
 		viewpanel.repaint();
 		viewpanel.setLayout(new BorderLayout());
-		viewpanel.setPreferredSize(new Dimension(250,150));
-								
-		viewpanel.add(new JButton("button1"), BorderLayout.NORTH);
-		viewpanel.add(new JButton("button2"), BorderLayout.SOUTH);
-		viewpanel.add(new JButton("button3"), BorderLayout.NORTH);
-//		viewpanel.add(new JButton("button4"), BorderLayout.EAST);
-//		viewpanel.add(new JButton("button5"), BorderLayout.CENTER);					
-				
-		generalpanel.doLayout();
-		viewpanel.doLayout();
-//		viewpanel.removeAll();
-	}
-	//Die Methode löscht zuerst das Panel, legt dann das FlowLayout 
-	//dafür fest und fügt die Elemente hinzu, aktualisiert dann die Layuts von general 
-	//und view Panels
-	
-	public void setFlowLayout() {
+		viewpanel.setPreferredSize(new Dimension(250, 150));
 		
+		//überprüfen jeden element von Array und wenn er eins ist 
+		// setzen die schaltenflache an die entsprechende Position 
+		
+		if(arr[0] == 1)
+			viewpanel.add(button1, BorderLayout.NORTH);
+		if(arr[1] == 1)
+			viewpanel.add(button2, BorderLayout.SOUTH);
+		if(arr[2] == 1)
+			viewpanel.add(button3, BorderLayout.WEST);
+		if(arr[3] == 1)
+			viewpanel.add(button4, BorderLayout.EAST);
+		if(arr[4] == 1)
+			viewpanel.add(button5, BorderLayout.CENTER);		
+
+		
+		viewpanel.doLayout();
+
+	}
+	// Die Methode löscht zuerst das Panel, legt dann das FlowLayout
+	// dafür fest und fügt die Elemente hinzu, aktualisiert dann die Layuts von
+	// view Panels
+
+	public void setFlowLayout() {
+
 		viewpanel.removeAll();
 		viewpanel.repaint();
-		viewpanel.setLayout(new FlowLayout());		
-		viewpanel.setPreferredSize(new Dimension(250,150));
+		viewpanel.setLayout(new FlowLayout());
+		viewpanel.setPreferredSize(new Dimension(250, 150));
+
+		viewpanel.add(button1);
+		viewpanel.add(button2);
+		viewpanel.add(button3);
+		viewpanel.add(button4);
+		viewpanel.add(button5);
 		
-		for(int i = 1; i <= 5; i++ ) 
-			viewpanel.add(new JButton("button " + i));
-		
-		generalpanel.doLayout();
 		viewpanel.doLayout();
-		
-		
+
+	}
+	
+	// Die Methode löscht zuerst das Panel, legt dann das GridLayout ensprechend
+		//	der Anzahl der übergebenen Zeilen und Spalten
+		// dafür fest und fügt die Elemente hinzu, aktualisiert dann die Layuts von
+		// view Panels
+	public void setGridLayout(int reihe, int spalte) {
+		viewpanel.removeAll();
+		viewpanel.repaint();
+		viewpanel.setLayout(new GridLayout(reihe, spalte));
+		viewpanel.setPreferredSize(new Dimension(250,150));		
+		 			
+			viewpanel.add(button1);
+			viewpanel.add(button2);
+			viewpanel.add(button3);
+			viewpanel.add(button4);
+			viewpanel.add(button5);			
+			
+		viewpanel.doLayout();
+			
 		
 	}
+
 	class MyListener implements ActionListener, ItemListener {
 
 		@Override
-		public void itemStateChanged(ItemEvent e) {
+		public void itemStateChanged(ItemEvent e) {			
 			
-				
-			}
-			
-			
-		
+		}
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			
+			//wählen das Layout
 			Object ausloeser = e.getSource();
-			if(ausloeser instanceof JRadioButton)
-				if(flow.isSelected()) {
+			if (ausloeser instanceof JRadioButton) {
+				if (flow.isSelected()) {
 					setEnableBorder(false);
 					setEnableGrid(false);
 					setFlowLayout();
 				}
-				if(border.isSelected()) {
+				if (border.isSelected()) {
 					setEnableBorder(true);
 					setEnableGrid(false);
-					setBorderLayout();
-															
+					int [] arr = {0,0,0,0,0};
+					setBorderLayout( arr);
+
 				}
-				if(grid.isSelected()) {
+				if (grid.isSelected()) {
 					setEnableBorder(false);
 					setEnableGrid(true);
-				}
-			if(ausloeser instanceof JComboBox) {
-				if(e.getActionCommand().equals("button1")) {
-					int selectedIndex = ((JComboBox<String>) ausloeser).getSelectedIndex();
+					//löschen das viewpanel 
+					viewpanel.removeAll();
+					viewpanel.repaint();
 					
-					if (selectedIndex == 0)
-						System.out.println("1 North");
-					if(selectedIndex == 1)
-						System.out.println("1 South");
-					if(selectedIndex == 2)
-						System.out.println("1 West");
-					if(selectedIndex == 3)
-						System.out.println("1 East");
-					if(selectedIndex == 4)
-						System.out.println("1 Center");
-				}
-				if(e.getActionCommand().equals("button2")) {
-					int selectedIndex = ((JComboBox<String>) ausloeser).getSelectedIndex();
 					
-					if (selectedIndex == 0)
-						System.out.println("2 North");
-					if(selectedIndex == 1)
-						System.out.println("2 South");
-					if(selectedIndex == 2)
-						System.out.println("2 West");
-					if(selectedIndex == 3)
-						System.out.println("2 East");
-					if(selectedIndex == 4)
-						System.out.println("2 Center");
-				}
-				if(e.getActionCommand().equals("button3")) {
-					int selectedIndex = ((JComboBox<String>) ausloeser).getSelectedIndex();
+					reihe.setSelectedIndex(0);
+					spalte.setSelectedIndex(0);
 					
-					if (selectedIndex == 0)
-						System.out.println("3 North");
-					if(selectedIndex == 1)
-						System.out.println("3 South");
-					if(selectedIndex == 2)
-						System.out.println("3 West");
-					if(selectedIndex == 3)
-						System.out.println("3 East");
-					if(selectedIndex == 4)
-						System.out.println("3 Center");
-				}
-				if(e.getActionCommand().equals("button4")) {
-					int selectedIndex = ((JComboBox<String>) ausloeser).getSelectedIndex();
-					
-					if (selectedIndex == 0)
-						System.out.println("4 North");
-					if(selectedIndex == 1)
-						System.out.println("4 South");
-					if(selectedIndex == 2)
-						System.out.println("4 West");
-					if(selectedIndex == 3)
-						System.out.println("4 East");
-					if(selectedIndex == 4)
-						System.out.println("4 Center");
 				}
 			}
+			if(ausloeser instanceof JComboBox) {
+				//nehmen die Anzahl der Zeilen und Spalten und übergeben sie an
+				//die Methode setGridLayout
+				int reihenMenge = reihe.getSelectedIndex();
+				int spalteMenge = spalte.getSelectedIndex();
 				
+				if (!(reihenMenge == 0 && spalteMenge == 0)) 					
+				setGridLayout(reihenMenge, spalteMenge);				
+				
+			}
+			// Wählen Sie aus, welche Elemente ausgewählt sind, und weisen Sie dem entsprechenden
+			//Element des Arrays den Wert 1 zu		
+			if (e.getActionCommand().equals("go")) {
+				int tempArray [] = {0,0,0,0,0};
+				if(north.isSelected())
+					tempArray[0] = 1;
+				if(south.isSelected())
+					tempArray[1] = 1;
+				if(west.isSelected())
+					tempArray[2] = 1;
+				if(east.isSelected())
+					tempArray[3] = 1;
+				if(center.isSelected())
+					tempArray[4] = 1;
+				
+					
+				setBorderLayout(tempArray);
+
+			}
+
 		}
-}
-		
-	
-	
+	}
 
 	public LayoutSpiel(String title) {
-		
+
 		super(title);
-		
+
 		generalpanel = new JPanel();
 		changepanel = createChangePanel();
 		borderpanel = createborderpanel();
 		gridpanel = creategridpanel();
 		viewpanel = createviewpanel();
-		setFlowLayout();
 		
+		button1 = new JButton("button 1");
+		button2 = new JButton("button 2");
+		button3 = new JButton("button 3");
+		button4 = new JButton("button 4");
+		button5 = new JButton("button 5");
+		
+		setFlowLayout();
+
 		generalpanel.add(changepanel);
 		generalpanel.add(borderpanel);
 		generalpanel.add(gridpanel);
 		generalpanel.add(viewpanel);
-		
-		
-		
-		
-		
-		setSize(950,200);
-		
+			
+		setSize(950, 250);
 		add(generalpanel);
-
 		setVisible(true);
-		
+
 	}
-	
+
 	public static void main(String[] args) {
-		
+
 		new LayoutSpiel("Layout Spiel");
 	}
 
